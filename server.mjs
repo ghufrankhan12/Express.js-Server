ad5ad75f7273992c4c5e5d4cca690ee6b144ab24
Ghufran Arain <ghufranqadri10@gmail.com>
2023-08-10 19:39:24 +0500

    express server

--- server.mjs
+++ server.mjs
@@ -2,13 +2,13 @@
 
 
 import express from 'express'
-// import cors from "cors"
+import cors from "cors"
 import path from 'path'
 const __dirname = path.resolve();
 
 
 const app = express()
-// app.use(cors())
+app.use(cors())
 
 
 app.get('/weather/:cityName', (req, res) => {
